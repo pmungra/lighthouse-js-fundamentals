0b5cd6a0d93b6c5d3c3e9d806be23839b09890ab
parth <parthpmungra@gmail.com>
2021-11-11 03:15:15 +0000

    Arrays - Ice cream!

--- ice-cream.js
+++ ice-cream.js
@@ -1,5 +1,15 @@
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
 
-iceCreamFlavours[5]="root beer";
+// Your code below here...
+
+iceCreamFlavours[5] = "root beer";
 console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+
+console.log(iceCreamFlavours.length);
